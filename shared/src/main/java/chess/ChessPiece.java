902eb53228c2e2c8259695f1210536d1ede0b211
Jeremy Beutler <jeremy.e.beutler@gmail.com>
2025-09-17 10:18:00 -0600

    Basic move functionality.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,7 @@
 package chess;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Represents a single chess piece
@@ -11,7 +11,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +35,66 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
+    }
+
+    public List<ChessMove> getPromotionMoves(ChessPosition position1, ChessPosition position2) {
+        List<ChessMove> moves = new ArrayList<>();
+        List<ChessPiece.PieceType> types = Arrays.asList(PieceType.QUEEN, PieceType.BISHOP, PieceType.KNIGHT, PieceType.ROOK);
+        for (ChessPiece.PieceType type : types) {
+            moves.add(new ChessMove(position1, position2, type));
+        }
+        // generate moves...
+        return moves;
+    }
+
+    public List<ChessMove> getMoves(List<ChessMove> validMoves, int[][] directionsArr, ChessBoard board, ChessPosition position, ChessPiece piece) {
+        for (int[] dir : directionsArr) {
+            int dx = dir[0], dy = dir[1];
+            int x = position.getColumn() + dx;
+            int y = position.getRow() + dy;
+            boolean inBounds = (x > 0 && y > 0 && x < 9 && y < 9);
+            if (inBounds) {
+                ChessPosition nextPos = new ChessPosition(y, x);
+                ChessPiece pieceAtPos = board.getPiece(nextPos);
+                if (pieceAtPos == null || (pieceAtPos.getTeamColor() != piece.getTeamColor())) {
+                    validMoves.add(new ChessMove(position, nextPos, null));
+                }
+            }
+        }
+        return validMoves;
+    }
+
+    public List<ChessMove> loopGetMoves(List<ChessMove> validMoves, int[][] directionsArr, ChessBoard board, ChessPosition position, ChessPiece piece) {
+        for (int[] dir : directionsArr) {
+            int dx = dir[0], dy = dir[1];
+            int x = position.getColumn() + dx;
+            int y = position.getRow() + dy;
+
+            while (x > 0 && y > 0 && x < 9 && y < 9) {
+                ChessPosition nextPos = new ChessPosition(y, x);
+                ChessPiece pieceAtPos = board.getPiece(nextPos);
+
+                if (pieceAtPos != null) {
+                    if (pieceAtPos.getTeamColor() != piece.getTeamColor()) {
+                        validMoves.add(new ChessMove(position, nextPos, null));
+                    }
+                    break;
+                }
+
+                validMoves.add(new ChessMove(position, nextPos, null));
+                x += dx;
+                y += dy;
+            }
+        }
+        return validMoves;
     }
 
     /**
@@ -48,6 +105,104 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        ChessPiece myPiece = board.getPiece(myPosition);
+        List<ChessMove> validMoves = new ArrayList<>();
+        int[][] knightDirections = {
+                {-1, 2},
+                {1, 2},
+                {-2, 1},
+                {2, 1},
+                {-2, -1},
+                {2,-1},
+                {-1,-2},
+                {1,-2}
+        };
+        int[][] diagonalDirections = {
+                {-1, 1}, // NW
+                {1, 1}, // NE
+                {-1, -1}, // SW
+                {1, -1}  // SE
+        };
+        int[][] orthogonalDirections = {
+                {0, 1}, // N
+                {0, -1}, // S
+                {1, 0}, // E
+                {-1, 0}  // W
+        };
+        int[][] allDirections = Stream.concat(
+                Arrays.stream(orthogonalDirections),
+                Arrays.stream(diagonalDirections)
+        ).toArray(int[][]::new);
+        if (myPiece.getPieceType() == PieceType.BISHOP) {
+            validMoves = loopGetMoves(validMoves, diagonalDirections, board, myPosition, myPiece);
+        } else if (myPiece.getPieceType() == PieceType.KING) {
+            validMoves = getMoves(validMoves, allDirections, board, myPosition, myPiece);
+        } else if (myPiece.getPieceType() == PieceType.KNIGHT) {
+            validMoves = getMoves(validMoves, knightDirections, board, myPosition, myPiece);
+        } else if (myPiece.getPieceType() == PieceType.PAWN) {
+            ChessGame.TeamColor myPieceColor = myPiece.getTeamColor();
+            int dy = (myPieceColor == ChessGame.TeamColor.WHITE) ? 1 : -1;
+            int startRow  = (myPieceColor == ChessGame.TeamColor.WHITE) ? 2 : 7;
+            int[] xOptions = {-1, 0, 1};
+
+            for (int dx : xOptions) {
+                int x = myPosition.getColumn() + dx;
+                int y = myPosition.getRow() + dy;
+                boolean inBounds = (x > 0 && y > 0 && x < 9 && y < 9);
+                boolean promotePawn = (
+                    (myPieceColor == ChessGame.TeamColor.WHITE && y == 8) ||
+                    (myPieceColor == ChessGame.TeamColor.BLACK && y == 1)
+                );
+                if (inBounds) {
+                    ChessPosition nextPos = new ChessPosition(y, x);
+                    ChessPiece pieceAtPos = board.getPiece(nextPos);
+                    if (dx == 0) {
+                        if (pieceAtPos == null) {
+                            if (promotePawn)  {
+                                validMoves.addAll(getPromotionMoves(myPosition, nextPos));
+                            } else {
+                                validMoves.add(new ChessMove(myPosition, nextPos, null));
+                                // Include moving 2 squares on first move
+                                if (myPosition.getRow() == startRow) {
+                                    y = y + dy;
+                                    nextPos = new ChessPosition(y, x);
+                                    pieceAtPos = board.getPiece(nextPos);
+                                    if (pieceAtPos == null) {
+                                        validMoves.add(new ChessMove(myPosition, nextPos, null));
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        if (pieceAtPos != null && pieceAtPos.getTeamColor() != myPieceColor) {
+                            if (promotePawn)  {
+                                validMoves.addAll(getPromotionMoves(myPosition, nextPos));
+                            } else {
+                                validMoves.add(new ChessMove(myPosition, nextPos, null));
+                            }
+                        }
+                    }
+                }
+            }
+        } else if (myPiece.getPieceType() == PieceType.QUEEN) {
+            validMoves = loopGetMoves(validMoves, allDirections, board, myPosition, myPiece);
+        } else if (myPiece.getPieceType() == PieceType.ROOK) {
+            validMoves = loopGetMoves(validMoves, orthogonalDirections, board, myPosition, myPiece);
+        }
+        return validMoves;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
